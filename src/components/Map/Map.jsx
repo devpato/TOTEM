037ebcc3a305db1a568bc91875b5923cb172168f
Patricio Vargas <pevargas2730@gmail.com>
2023-07-09 11:25:40 +0200

    gmaps working

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,28 @@
-import { Map } from '@googlemaps/react-wrapper'
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function GMap() {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+
   return (
-    <div>
-      <Map apiKey="AIzaSyAFIzezfPUTRW15phnO5QTo99Vf_AmcD5Yc" />
+    // Important! Always set the container height explicitly
+    <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyByVGlyxbZOCXx-J_sTzSIFp6klHQX7XB4' }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+      </GoogleMapReact>
     </div>
-  )
+  );
 }
 
-export default GMap
+export default GMap;
